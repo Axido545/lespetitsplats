1bd51bed3defc54d39cb32ce256eb96573317a51
axido545 <axido545@gmail.com>
2023-08-08 19:25:48 +0200

    maj

--- js/script/index.js
+++ js/script/index.js
@@ -66,43 +66,23 @@ export function displayRecipes() {
         ingredientSubItem.setAttribute("class","fw-light")
         ingredientSubItem.textContent = `${quantity} ${unit}`;
         ingredientBlock.appendChild(ingredientSubItem);
-
-
       }
-
-  
-
     }
-
-
     const timeParagraph = document.createElement("p");
     timeParagraph.classList.add("reciepe-time");
     timeParagraph.textContent = `${recipe.time} min`;
     wrapTextRecipe.appendChild(timeParagraph);
-
-    // const applianceParagraph = document.createElement("p");
-    // applianceParagraph.textContent = `Appareil: ${recipe.appliance}`;
-    // wrapTextRecipe.appendChild(applianceParagraph);
-
-    // const ustensilsParagraph = document.createElement("p");
-    // ustensilsParagraph.textContent = `Ustensils: ${recipe.ustensils.join(", ")}`;
-    // wrapTextRecipe.appendChild(ustensilsParagraph);
   }
-
   // Calcule du nombre de recettes
   const numberOfRecipes = recipes.length;
-
   // Affichage le nombre de recettes dans un élément HTML avec l'ID "recipeCount"
   const recipeCountElement = document.getElementById("recipeCount");
   if (recipeCountElement) {
     recipeCountElement.textContent = `${numberOfRecipes} recettes`;
   }
-
-
 }
 // Appeler la fonction pour afficher les recettes lorsque la page est chargée
-window.onload = displayRecipes;
-
+displayRecipes();
 
 const searchBtn = document.querySelector(".search-btn");
 const whiteGlass = document.querySelector(".glass-white")
@@ -110,161 +90,47 @@ const blackGlass = document.querySelector(".glass-black")
 whiteGlass.style.display ="block"
 blackGlass.style.display ="none"
 
-searchBtn.addEventListener("mouseover", function(){
+searchBtn.addEventListener("mouseover", function(event){
+  event.preventDefault(); // Empêche le rafraîchissement de la page
 whiteGlass.style.display ="none"
 blackGlass.style.display ="block"
 })
 
-searchBtn.addEventListener("mouseout", function(){
+searchBtn.addEventListener("mouseout", function(event){
+  event.preventDefault(); // Empêche le rafraîchissement de la page
   whiteGlass.style.display ="block"
   blackGlass.style.display ="none"
-  
-  
   })
 
 
-// Gestionnaire d'événement pour le formulaire de recherche
-document.getElementById('searchForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-  const searchInput = document.getElementById('searchInput').value.trim();
+// Gestionnaire d'événement pour le champ de recherche en temps réel
+document.getElementById('searchInput').addEventListener('change', function (event) {
+  event.preventDefault(); // Empêche le rafraîchissement de la page
+  const searchInput = event.target.value.trim();
+  
   if (searchInput.length >= 3) {
     const filteredRecipes = searchRecipes(searchInput);
     displayFilteredRecipes(filteredRecipes);
+
+    if(filteredRecipes.length === 0) {
+      const recipeContainer = document.getElementById("recipeContainer");
+      const key = document.getElementById('searchInput')
+  
+      recipeContainer.textContent = `« Aucune recette ne contient « ${key.value} » vous pouvez chercher « tarte aux pommes », « poisson », etc.` ;
+  
+    }
+
   } else {
-    console.log('Veuillez saisir au moins 3 caractères.');
+    recipeContainer.textContent = "Veuillez saisir au moins 3 caractères.";
   }
+
+
+  
+
+
 });
-
-
-
 
 searchRecipes();
 displayFilteredRecipes();
 
 
-// import { searchRecipes, displayFilteredRecipes } from '../utils/filtre.js';
-// import {recipes}  from '../data/recipes.js';
-
-
-// /// Fonction pour afficher les recettes dans l'élément avec l'ID "recipeContainer"
-// function displayRecipes() {
-//   const recipeContainer = document.getElementById("recipeContainer");
-//   recipeContainer.classList.add("gallery-recipes");
-
-//   for (let recipe of recipes) {
-//     const articleRecipe = document.createElement('article');
-//     articleRecipe.classList.add("recipe-article", "position-relative", "col-4"); // Ajout des classes Bootstrap pour la mise en forme
-//     recipeContainer.appendChild(articleRecipe);
-
-//     const imgRecipe = document.createElement("img");
-//     imgRecipe.classList.add("img-recette", "img-fluid"); // Ajout des classes Bootstrap pour la mise en forme
-//     imgRecipe.setAttribute("src", `asset/imgs_recettes/${recipe.image}`);
-//     imgRecipe.setAttribute("alt", recipe.name);
-//     articleRecipe.appendChild(imgRecipe);
-
-//     const wrapTextRecipe = document.createElement("section");
-//     wrapTextRecipe.classList.add("text-container", "p-3"); // Ajout des classes Bootstrap pour la mise en forme
-//     articleRecipe.appendChild(wrapTextRecipe);
-
-//     const titleRecipe = document.createElement("h2");
-//     titleRecipe.classList.add("reciepe-name", "mb-3"); // Ajout des classes Bootstrap pour la mise en forme
-//     titleRecipe.textContent = recipe.name;
-//     wrapTextRecipe.appendChild(titleRecipe);
-
-//     const subTitlRecipeRec = document.createElement("h3");
-//     subTitlRecipeRec.classList.add("reciepe-subtitle", "mb-2"); // Ajout des classes Bootstrap pour la mise en forme
-//     subTitlRecipeRec.textContent = "RECETTE";
-//     wrapTextRecipe.appendChild(subTitlRecipeRec);
-
-//     const recipeDesc = document.createElement("p");
-//     recipeDesc.textContent = recipe.description;
-//     recipeDesc.setAttribute("class","recipe-desc")
-//     wrapTextRecipe.appendChild(recipeDesc);
-
-
-
-//     const subTitlRecipeIng = document.createElement("h3");
-//     subTitlRecipeIng.classList.add("reciepe-subtitle", "mb-2"); // Ajout des classes Bootstrap pour la mise en forme
-//     subTitlRecipeIng.textContent = "INGREDIENTS";
-//     wrapTextRecipe.appendChild(subTitlRecipeIng);
-
-//     const ingredientsList = document.createElement("section");
-//     ingredientsList.setAttribute("class","row row-cols-2")
-//     wrapTextRecipe.appendChild(ingredientsList);
-
-//     for (const ingredient of recipe.ingredients) {
-//       const { ingredient: name, quantity, unit = "" } = ingredient;
-
-//       const ingredientBlock = document.createElement("div")
-//       ingredientBlock.setAttribute("class","col")
-//       ingredientsList.appendChild(ingredientBlock);
-
-
-//       const ingredientItem = document.createElement("p");
-//       ingredientItem.setAttribute("class","ingredient-Item-name")
-//       ingredientItem.textContent = `${name}`;
-//       ingredientBlock.appendChild(ingredientItem);
-
-//       if(quantity) {
-//         const ingredientSubItem = document.createElement("p")
-//         ingredientSubItem.setAttribute("class","fw-light")
-//         ingredientSubItem.textContent = `${quantity} ${unit}`;
-//         ingredientBlock.appendChild(ingredientSubItem);
-//       }
-//     }
-//     const timeParagraph = document.createElement("p");
-//     timeParagraph.classList.add("reciepe-time");
-//     timeParagraph.textContent = `${recipe.time} min`;
-//     wrapTextRecipe.appendChild(timeParagraph);
-
-//   // Calcule du nombre de recettes
-//   const numberOfRecipes = recipes.length;
-
-//   // Affichage le nombre de recettes dans un élément HTML avec l'ID "recipeCount"
-//   const recipeCountElement = document.getElementById("recipeCount");
-//   if (recipeCountElement) {
-//     recipeCountElement.textContent = `${numberOfRecipes} recettes`;
-//   }
-// }
-// // Appeler la fonction pour afficher les recettes lorsque la page est chargée
-// window.onload = displayRecipes;
-
-
-// const searchBtn = document.querySelector(".search-btn");
-// const whiteGlass = document.querySelector(".glass-white")
-// const blackGlass = document.querySelector(".glass-black")
-// whiteGlass.style.display ="block"
-// blackGlass.style.display ="none"
-
-// searchBtn.addEventListener("mouseover", function(){
-// whiteGlass.style.display ="none"
-// blackGlass.style.display ="block"
-// })
-
-// searchBtn.addEventListener("mouseout", function(){
-//   whiteGlass.style.display ="block"
-//   blackGlass.style.display ="none"
-  
-  
-//   })
-
-
-// // Gestionnaire d'événement pour le formulaire de recherche
-// document.getElementById('searchForm').addEventListener('submit', function (event) {
-//   event.preventDefault();
-//   const searchInput = document.getElementById('searchInput').value.trim();
-//   if (searchInput.length >= 3) {
-//     const filteredRecipes = searchRecipes(searchInput);
-//     displayFilteredRecipes(filteredRecipes);
-//   } else {
-//     console.log('Veuillez saisir au moins 3 caractères.');
-//   }
-// });
-
-// displayRecipes()
-
-// // window.onload = function() {
-// //   const initialKeyword =  document.getElementById('searchInput').value.trim();
-// //   const filteredRecipes = searchRecipes(initialKeyword);
-// //   displayFilteredRecipes(filteredRecipes);
-// // }}
--- js/utils/filtre.js
+++ js/utils/filtre.js
@@ -1,5 +1,10 @@
 import {recipes}  from '../data/recipes.js';
 
+
+
+// // Afficher les recettes filtrées dans la console
+// displayFilteredRecipes(filteredRecipes);
+
 // Fonction pour effectuer la recherche en fonction de la saisie de l'utilisateur
 export function searchRecipes(keyword) {
   const lowerCaseKeyword = keyword.toLowerCase();
@@ -32,7 +37,6 @@ export function displayFilteredRecipes(filteredRecipes) {
       recipeCount.textContent = `${filteredRecipes.length} recettes`;
 
 
-      
       const recipeName = recipeItem.querySelector('.reciepe-name').textContent.trim();
       const recipeIngredients = recipeItem.querySelector('.ingredient-Item-name').textContent.trim();
       const recipeDescription = recipeItem.querySelector('.recipe-desc').textContent.trim();
@@ -55,66 +59,5 @@ export function displayFilteredRecipes(filteredRecipes) {
         recipeItem.style.display = 'none';
       }
     }
+  
   }
-// const keyword = document.getElementById('searchInput').value.trim();
-
-
-// // Fonction pour effectuer la recherche en fonction de la saisie de l'utilisateur
-// export function searchRecipes(keyword) {
-// // Keyword représente le mot clé saisi par l'utlisateur
-
-
-//   // convertir le mot clé en minuscule
-//   const lowerCaseKeyword = keyword.toLowerCase();
-
-//   // filtrer les recettes en fonction du mot clé saisi
-//   const filteredRecipes = recipes.filter(recipe => {
-//     const lowerCaseName = recipe.name.toLowerCase();
-//     const lowerCaseIngredients = recipe.ingredients.join(' ').toLowerCase();
-//     const lowerCaseDescription = recipe.description.toLowerCase();
-
-//     return (
-//       lowerCaseName.includes(lowerCaseKeyword) ||
-//       lowerCaseIngredients.includes(lowerCaseKeyword) ||
-//       lowerCaseDescription.includes(lowerCaseKeyword)
-//     );
-//   });
-
-//   return filteredRecipes;
-// }
-
-
-
-// export function displayFilteredRecipes(filteredRecipes) {
-//     console.log(filteredRecipes);
-  
-//     const allRecipeItems = document.querySelectorAll('.recipe-article');
-  
-//     for (const recipeItem of allRecipeItems) {
-//       const recipeCount = document.querySelector("#recipeCount")
-//       recipeCount.textContent = `${filteredRecipes.length} recettes`;
-    
-
-//       const recipeName = recipeItem.querySelector('.reciepe-name').textContent.trim();
-//       const recipeIngredients = recipeItem.querySelector('.ingredient-Item-name').textContent.trim();
-//       const recipeDescription = recipeItem.querySelector('.recipe-desc').textContent.trim();
-  
-//       const isRecipeIncluded = filteredRecipes.filter(recipe => {
-//         const lowerCaseName = recipe.name.toLowerCase();
-//         const lowerCaseIngredients = recipe.ingredients.join(' ').toLowerCase();
-//         const lowerCaseDescription = recipe.description.toLowerCase();
-  
-//         return (
-//           lowerCaseName === recipeName.toLowerCase() ||
-//           lowerCaseIngredients.includes(recipeIngredients.toLowerCase()) ||
-//           lowerCaseDescription.includes(recipeDescription.toLowerCase())
-//         );
-//       }).length > 0;
-  
-//       if (isRecipeIncluded) {
-//         recipeItem.style.display = 'block';
-//       } else {
-//         recipeItem.style.display = 'none';
-//       }
-//     }
-//   }

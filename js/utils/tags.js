37bc17deb10c29598748301123e3dbe980921a6b
axido545 <axido545@gmail.com>
2023-11-29 11:56:57 +0100

    maj event suggestion

--- js/utils/tags.js
+++ js/utils/tags.js
@@ -69,6 +69,7 @@ export async function removeTag(tagText, suggestion) {
     const existingImage = suggestion.querySelector(".close-suggestion");
     if (existingImage && existingImage.parentNode) {
       existingImage.parentNode.removeChild(existingImage);
+      console.log("c'est ici");
     }
   }
 
